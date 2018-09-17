d956fc7f84e9891b9d466f767678a9dc7199fd91
Chris Frey <chrisdfrey@gmail.com>
2018-09-17 17:02:06 -0400

    ouput solutions to files

--- exporter.py
+++ exporter.py
@@ -1,19 +1,38 @@
 import requests
 import re
+import os
+import html
 
 php_session_id = "" # change this for your login
 cookies = {"PHPSESSID": php_session_id}
 
-r = requests.get("https://www.codeabbey.com/index/user_profile", cookies=cookies)
-
 solution_re = re.compile(r"\"/index/task_solution?([^\"]+)\"")
+code_re = re.compile(r"<code[^>]+>(.*?)</code>", re.DOTALL)
+task_re = re.compile("task=([^&]+)")
+
+r = requests.get("https://www.codeabbey.com/index/user_profile", cookies=cookies)
 solutions = solution_re.findall(r.text)
 
-code_re = re.compile(r"<code[^>]+>(.*?)</code>", re.DOTALL)
+output_folder = "C:\\codeabbey\\output\\"
+
+if not os.path.isdir(output_folder):
+    os.makedirs(output_folder)
 
 for solution in solutions:
     url = "https://www.codeabbey.com/index/task_solution" + solution
     r = requests.get(url, cookies=cookies)
 
-    code = code_re.search(r.text).group(1)
-    print(code)
+    code_match = code_re.search(r.text)
+    if code_match is None:
+        continue
+
+    code = code_match.group(1)
+    code = html.unescape(code)
+    code = code.replace("\r", "")
+
+    task = task_re.search(solution).group(1)
+
+    output_path = os.path.join(output_folder, task + ".py")
+    f = open(output_path,'w')
+    f.write(code)
+    f.close()
