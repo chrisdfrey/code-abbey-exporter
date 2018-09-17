ca5e90755542c09d35773681137c4a335e0081ca
Chris Frey <chrisdfrey@gmail.com>
2018-09-17 12:54:12 -0400

    fetching and printing solutions

--- exporter.py
+++ exporter.py
@@ -1,7 +1,19 @@
 import requests
+import re
 
 php_session_id = "" # change this for your login
+cookies = {"PHPSESSID": php_session_id}
 
-r = requests.get("https://www.codeabbey.com/index/user_profile", cookies={"PHPSESSID": php_session_id})
+r = requests.get("https://www.codeabbey.com/index/user_profile", cookies=cookies)
 
-print(r.text)
+solution_re = re.compile(r"\"/index/task_solution?([^\"]+)\"")
+solutions = solution_re.findall(r.text)
+
+code_re = re.compile(r"<code[^>]+>(.*?)</code>", re.DOTALL)
+
+for solution in solutions:
+    url = "https://www.codeabbey.com/index/task_solution" + solution
+    r = requests.get(url, cookies=cookies)
+
+    code = code_re.search(r.text).group(1)
+    print(code)
